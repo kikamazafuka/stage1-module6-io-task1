2adbec6abccf98d0343c7d8d71bdab2720884b2e
Artur Semenas <uncelfuka@gmail.com>
2023-11-22 00:30:50 +0100

    Stage 1 IO File uploading (exception)

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,6 +2,7 @@ package com.epam.mjc.io;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,7 +10,7 @@ import java.util.Map;
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
+    public Profile getDataFromFile(File file){
         Profile profile = new Profile();
         Map<String, String> map = new HashMap<>();
         try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))) {
@@ -18,9 +19,11 @@ public class FileReader {
                 String[] str = line.split(":", 2);
                 map.put(str[0].strip(), str[1].strip());
             }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.err.println("File not found" + fileNotFoundException);;
+        }
+        catch (IOException ioException){
+            System.err.println("IO exception" + ioException);
         }
         profile.setAge(Integer.parseInt(map.get("Age")));
         profile.setEmail(map.get("Email"));
